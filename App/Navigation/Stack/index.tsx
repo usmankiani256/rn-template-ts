e64339f474ac6639f57fa45fa91565d02230b3d8
Usman Kiani <usmankiani256@gmail.com>
2021-08-27 23:07:10 +0500

    :heavy_plus_sign: add react-navigation

---
+++ App/Navigation/Stack/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+const StackNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  )
+}
+
+export default StackNavigator
