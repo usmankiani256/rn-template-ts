d981ad422419587d127f71cd1ab0e9f4ef3027a3
Usman Kiani <usmankiani256@gmail.com>
2021-08-30 03:29:26 +0500

    :sparkles: consume graphql client at root

--- App.tsx
+++ App.tsx
@@ -4,15 +4,20 @@ import { RootNavigator } from '@Navigation'
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from '@Redux'
 
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@Api'
+
 import { Provider as PaperProvider } from 'react-native-paper'
 import { theme } from '@Theme'
 
 const App: React.FC = () => {
   return (
     <ReduxProvider store={store}>
-      <PaperProvider theme={theme}>
-        <RootNavigator />
-      </PaperProvider>
+      <ApolloProvider client={client}>
+        <PaperProvider theme={theme}>
+          <RootNavigator />
+        </PaperProvider>
+      </ApolloProvider>
     </ReduxProvider>
   )
 }
