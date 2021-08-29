13cd498206f12d84ade284d9dec1a116bb47d0cc
Usman Kiani <usmankiani256@gmail.com>
2021-08-30 01:13:43 +0500

    :zap: add stack navigator and welcome screen

--- App/Navigation/Stack/index.tsx
+++ App/Navigation/Stack/index.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import useStyles from './styles'
+
+import { WelcomeScreen } from '@Views'
+
+const Stack = createNativeStackNavigator()
 
 const StackNavigator: React.FC = () => {
+  const { headerHidden } = useStyles()
+
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          {...headerHidden}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
