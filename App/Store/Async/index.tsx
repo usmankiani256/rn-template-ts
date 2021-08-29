77d7af7ecf2c9010b9a1a78348e3d91596af0785
Usman Kiani <usmankiani256@gmail.com>
2021-08-30 02:25:49 +0500

    :sparkles: init Async module

---
+++ App/Store/Async/index.tsx
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-community/async-storage'
+import Item from './items'
+
+export type AsyncItem = string
+
+const useAsync = () => {
+  async function setItem(item: AsyncItem, data: JSON) {
+    let parsedData = JSON.stringify(data)
+
+    await AsyncStorage.setItem(item, parsedData)
+    console.debug(`Async:\nItem "${item}" set to ${data}`)
+  }
+
+  async function getItem(item: AsyncItem) {
+    const data = await AsyncStorage.getItem(item)
+    return data ? JSON.parse(data) : null
+  }
+
+  async function removeItem(item: AsyncItem) {
+    return await AsyncStorage.removeItem(item)
+  }
+
+  return { getItem, setItem, removeItem, Item }
+}
+
+export default useAsync
