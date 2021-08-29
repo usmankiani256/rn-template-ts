dad3a111df255eae4fae83a721910011766bceac
Usman Kiani <usmankiani256@gmail.com>
2021-08-30 02:01:04 +0500

    :sparkles: add Theme file

---
+++ App/Theme/index.tsx
@@ -0,0 +1,14 @@
+export const Colors = {
+  // Common
+  transparent: 'rgba(0,0,0,0)',
+  text: '#3f3f3f',
+  tintGrey: '#777b',
+  foreground: '#FFFFFF',
+  background: '#f5f5f5',
+  success: '#28a745',
+  error: '#dc3545',
+
+  // App Specific
+  primary: 'teal',
+  secondary: 'green',
+}
