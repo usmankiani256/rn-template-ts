993f76fca16bb03d4ff85d4d52091de6a53b5cc9
Usman Kiani <usmankiani256@gmail.com>
2021-08-30 02:33:30 +0500

    :sparkles: init Redux module

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { RootNavigator } from '@Navigation'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@Redux'
 
 const App: React.FC = () => {
-  return <RootNavigator />
+  return (
+    <ReduxProvider store={store}>
+      <RootNavigator />
+    </ReduxProvider>
+  )
 }
 
 export default App
