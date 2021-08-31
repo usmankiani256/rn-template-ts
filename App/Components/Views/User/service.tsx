00450ba1b217305ab8da69055b6f36361d358312
Usman Kiani <usmankiani256@gmail.com>
2021-09-01 03:33:32 +0500

    :beers: call and test fetchRandomUser flow

--- App/Components/Views/User/service.tsx
+++ App/Components/Views/User/service.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import {} from 'react-native'
 import { StackScreenProps } from '@Navigation/Stack/types'
+import { fetchRandomUser } from '@Api/Core'
 
 const useService = (props: StackScreenProps) => {
+  const fetch = async () => {
+    try {
+      const data = await fetchRandomUser()
+      console.log('Users', data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  fetch()
+
   return {}
 }
 
