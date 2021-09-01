13f5e9e4a9408fc5e0943d139881481ec15c2508
usmankiani256 <74454275+usmankiani256@users.noreply.github.com>
2021-09-01 19:37:49 +0500

    :sparkles: add User reducer to store

--- App/Store/Redux/index.tsx
+++ App/Store/Redux/index.tsx
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import User from './User'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { User },
 })
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
