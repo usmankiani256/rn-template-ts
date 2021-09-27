c0d068ca30372dbc0d8857b34654c42659f528c3
Muhammad Usman Kiani <usmankiani256@gmail.com>
2021-09-27 14:00:31 +0500

    :art: change Hook to Class

--- App/Store/Async/index.tsx
+++ App/Store/Async/index.tsx
@@ -1,27 +1,27 @@
-import AsyncStorage from '@react-native-community/async-storage'
+import RNAsyncStorage from '@react-native-community/async-storage'
 import Item from './items'
 
 export type AsyncItem = string
 
-// TODO: Shift it back to class and remove it as a custom hook
-const useAsync = () => {
-  async function setItem(item: AsyncItem, data: JSON) {
-    let parsedData = JSON.stringify(data)
+class AsyncStorage {
+  Item = Item
 
-    await AsyncStorage.setItem(item, parsedData)
+  async setItem(item: AsyncItem, data: JSON | boolean) {
+    let parsedData = JSON.stringify(data)
+    await RNAsyncStorage.setItem(item, parsedData)
     console.debug(`Async:\nItem "${item}" set to ${data}`)
   }
 
-  async function getItem(item: AsyncItem) {
-    const data = await AsyncStorage.getItem(item)
+  async getItem(item: AsyncItem) {
+    const data = await RNAsyncStorage.getItem(item)
     return data ? JSON.parse(data) : null
   }
 
-  async function removeItem(item: AsyncItem) {
-    return await AsyncStorage.removeItem(item)
+  async removeItem(item: AsyncItem) {
+    return await RNAsyncStorage.removeItem(item)
   }
-
-  return { getItem, setItem, removeItem, Item }
 }
 
-export default useAsync
+let Async = new AsyncStorage()
+
+export default Async
